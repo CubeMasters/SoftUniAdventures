9460af9b1de9b9296e474b99cd55a6ea727e2fe4
mhristoforova <mhristoforova@gmail.com>
2016-03-12 09:49:18 +0200

    Menu Stuff Added

---
+++ Assets/Scripts/Menu/Character.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class Character
+	{
+		public Character ()
+		{
+		}
+	}
+}
+
---
+++ Assets/Scripts/Menu/PlayScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayScript : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(GUI.Button == Button1)
+		{
+			Application.Quit();
+		}
+	}
+}
